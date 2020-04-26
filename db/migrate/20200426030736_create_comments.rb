e000ff8d3c1c29e5be4a8ae662ec99aa2949c9c0
Nicky <nickyliu91@gmail.com>
2020-04-25 23:16:31 -0400

    added seed data and fixed comment to have post id

--- db/migrate/20200426030736_create_comments.rb
+++ db/migrate/20200426030736_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :content
       t.intger :account_id
+      t.intger :post_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+yugi = Account.create(name: "Yugi", password: "aaa")
+kaiba = Account.create(name: "Kaiba", password: "bbb")
+marik = Account.create(name: "Marik", password: "ccc")
+
+dueling = Post.create(title: "Dueling", content: "Dueling is awesome!", account_id: yugi.id)
+bestMonster = Post.create(title: "Best Monster", content: "Blue-Eyes is the best monster!!", account_id: kaiba.id)
+
+comment1 = Comment.create(content: "You all suck", account_id: marik.id, post_id: dueling.id)
+comment2 = Comment.create(content: "Dark Magician is the best!", account_id: yugi.id, post_id: bestMonster.id)
