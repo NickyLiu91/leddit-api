3dc3df1815c950e0b0cb657518617ade3793d134
Nicky <nickyliu91@gmail.com>
2020-05-03 22:55:42 -0400

    added comment controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::CommentsController < ApplicationController
+
+  before_action :find_comment, only: [:show]
+
+  def index
+    @comments = Comment.all
+    render json: @comments
+  end
+
+  def show
+    render json: @comment
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+    render json: @comment
+  end
+
+  def edit
+    @comment = Comment.find_by(id: params[:id])
+  end
+
+  def update
+    @player = Player.find_by(id: params[:id])
+    @player.update(player_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def find_comment
+    @comment = Comment.find(params[:id])
+  end
+
 end
