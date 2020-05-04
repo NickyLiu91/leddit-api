d1a62aa1a549afeaf80d4d99ed0eca0169a0ac04
Nicky <nickyliu91@gmail.com>
2020-05-03 22:56:53 -0400

    added controller for replies

--- app/controllers/api/v1/replies_controller.rb
+++ app/controllers/api/v1/replies_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::RepliesController < ApplicationController
+
+  before_action :find_reply, only: [:show]
+
+  def index
+    @replys = Reply.all
+    render json: @replys
+  end
+
+  def show
+    render json: @reply
+  end
+
+  def new
+    @reply = Reply.new
+  end
+
+  def create
+    @reply = Reply.create(reply_params)
+    render json: @reply
+  end
+
+  def edit
+    @reply = Reply.find_by(id: params[:id])
+  end
+
+  def update
+    @player = Player.find_by(id: params[:id])
+    @player.update(player_params)
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:content)
+  end
+
+  def find_reply
+    @reply = Reply.find(params[:id])
+  end
+
 end
