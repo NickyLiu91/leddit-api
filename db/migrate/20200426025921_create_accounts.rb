33acb9550501a48f991d585dfcd38800d7f8a7d1
Nicky <nickyliu91@gmail.com>
2020-06-19 23:31:14 -0400

    fixed password digest on schema

--- db/migrate/20200426025921_create_accounts.rb
+++ db/migrate/20200426025921_create_accounts.rb
@@ -2,7 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
       t.string :name
-      t.string :password
+      t.string :password_digest
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_030736) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
