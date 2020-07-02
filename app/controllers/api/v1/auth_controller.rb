b63df174988d4d9cc00c06242a5f14ce38220003
Nicky <nickyliu91@gmail.com>
2020-07-01 23:05:35 -0400

    added auth controller

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::AuthController < ApplicationController
+  skip_before_action :authorized, only: %i[create]
+
+  def create
+    @account = Account.find_by(accountname: account_login_params[:accountname])
+    #Account#authenticate comes from BCrypt
+    if @account && @account.authenticate(account_login_params[:password])
+      # encode token comes from ApplicationController
+      token = encode_token({ account_id: @account.id })
+      render json: { account: AccountSerializer.new(@account), jwt: token }, status: :accepted
+    else
+      render json: { message: 'Invalid accountname or password' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def account_login_params
+    # params { account: {accountname: 'Chandler Bing', password: 'hi' } }
+    params.require(:account).permit(:accountname, :password)
+  end
+end
