a1f777c92159f1be35572069af044ca754b891fc
Nicky <nickyliu91@gmail.com>
2022-02-28 06:13:10 -0500

    changed votes to show ids rather then objects

--- app/serializers/commentVote_serializer.rb
+++ app/serializers/commentVote_serializer.rb
@@ -1,5 +1,5 @@
 class CommentvoteSerializer < ActiveModel::Serializer
-  attributes :account, :comment, :like
+  attributes :account_id, :comment_id, :like
 
   belongs_to :account
   belongs_to :comment
--- app/serializers/postVote_serializer.rb
+++ app/serializers/postVote_serializer.rb
@@ -1,5 +1,5 @@
 class PostvoteSerializer < ActiveModel::Serializer
-  attributes :account, :post, :like
+  attributes :account_id, :post_id, :like
 
   belongs_to :account
   belongs_to :post
